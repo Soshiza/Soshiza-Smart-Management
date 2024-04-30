e7b250f3cf2f476f97be2e8181f2e103395c0727
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-04-30 18:39:37 -0400

    The login presentation has been changed, and an alert has been added for incorrect login attempts

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,15 @@
-"use client";
+'use client'
 
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import { signInWithEmailAndPassword } from 'firebase/auth'; // Importa la función de autenticación de Firebase
-import { authentication } from '@/config/firebase'; // Importa la instancia de autenticación de Firebase
+import { signInWithEmailAndPassword } from 'firebase/auth'; 
+import { authentication } from '@/config/firebase';
+import Image from 'next/image'; 
 
 const LoginPage = () => {
   const router = useRouter();
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,28 +17,23 @@ const LoginPage = () => {
     const password = e.target.password.value;
 
     try {
-      // Autenticar al usuario con Firebase
       await signInWithEmailAndPassword(authentication, email, password);
-      // Redirigir al usuario a la página principal después de iniciar sesión
       router.push('/main');
     } catch (error) {
-      // Manejar errores de autenticación
       console.error('Error de autenticación:', error.message);
+      setShowAlert(true);
     }
   };
 
-  // Resto del código
   useEffect(() => {
     if (!router.isReady) return;
-    // Puedes realizar cualquier lógica adicional de inicialización aquí
+    
   }, [router.isReady]);
 
   return (
     <section className="flex items-center justify-center min-h-screen glass-window">
       <div className="container mx-auto">
-        {/* Contenedor para pantalla grande */}
         <div className="lg:flex lg:justify-center lg:border  lg:p-8 lg:rounded-lg">
-          {/* Primer panel */}
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
@@ -45,21 +42,20 @@ const LoginPage = () => {
             className="glass-window rounded-lg shadow-lg px-8 py-10 max-w-md w-full lg:w-auto mb-4 lg:mb-0 lg:mr-4"
           >
             <h2 className="text-2xl font-bold mb-4">
-              Bienvenido al gestor de tienda virtual
+              Bienvenido a Soshiza Smart Management
             </h2>
             <p className="text-gray-600 mb-4">
-              Aquí encontrarás todo lo que necesitas para gestionar tu tienda
-              virtual de forma eficiente.
+              Aquí encontrarás todo lo que necesitas para gestionar tu negocio, almacén o pyme de forma eficiente.
             </p>
             <p className="text-gray-600">
-              si necesita ayuda por favor contactarse a{' '}
+              Si necesitas ayuda por favor contactarse a{' '}
               <a
                 className="text-cyan-500"
                 href="mailto:soshiza.agency@gmail.com"
               >
                 soshiza.agency@gmail.com
               </a>{' '}
-              o visite nuestra pagina web{' '}
+              o visite nuestra página web{' '}
               <a
                 className="text-cyan-500"
                 href="http://www.soshiza.com"
@@ -69,9 +65,17 @@ const LoginPage = () => {
                 www.soshiza.com
               </a>
             </p>
+            <div className="flex justify-center">
+              <Image 
+                src="/soshiza.png" 
+                alt="soshiza smart management" 
+                width={200} 
+                height={200} 
+                style={{ filter: 'invert(100%) sepia(100%) saturate(0%) hue-rotate(150deg)' }}
+              />
+            </div>
           </motion.div>
 
-          {/* Segundo panel */}
           <motion.div
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
@@ -80,7 +84,7 @@ const LoginPage = () => {
             className="glass-window rounded-lg shadow-lg px-8 py-10 max-w-md w-full lg:w-auto"
           >
             <h2 className="text-2xl font-bold mb-4">
-              Gestor de Tienda Virtual de Bazar al Sol
+              Gestor de Negocios Soshiza Smart Management
             </h2>
             <p className="text-gray-600 mb-4">
               Por favor, para continuar, ingrese su email y contraseña.
@@ -118,6 +122,16 @@ const LoginPage = () => {
           </motion.div>
         </div>
       </div>
+      
+      {showAlert && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-8">
+            <h2 className="text-2xl font-bold mb-4 text-red-500">Error de autenticación</h2>
+            <p className="text-gray-600">El correo o la contraseña ingresados son incorrectos. Por favor, inténtelo nuevamente.</p>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md mt-4" onClick={() => setShowAlert(false)}>Cerrar</button>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
